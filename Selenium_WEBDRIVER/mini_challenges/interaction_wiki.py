88211a1813bf3742e769a0307b944e78e5065c38
AFK-08 <ahmadfkwj@gmail.com>
2026-08-07 11:12:24 +0500

    Searching through the wikipedia

--- Selenium_WEBDRIVER/mini_challenges/interaction_wiki.py
+++ Selenium_WEBDRIVER/mini_challenges/interaction_wiki.py
@@ -1,14 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach",True)
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
+driver.fullscreen_window()
 
-article_numbers = driver.find_element(By.ID,value="mwDw")
-print(article_numbers.text)
+## 1. Printing no of Articles:
+# article_numbers = driver.find_element(By.ID,value="mwDw")
+# print(article_numbers.text)
+
+
+## 2. Finding and Clicking on Links:
+
+# portals = driver.find_element(By.LINK_TEXT,value="Community portal")
+# portals.click()
+
+## 3. Searching through the page :
+
+search_wiki = driver.find_element(By.NAME, value="search")
+search_wiki.send_keys("Python", Keys.ENTER)
+
+
+
+
+
 
 
 
