4cb65516a0024ec3691d1a721f6c617657329201
AFK-08 <ahmadfkwj@gmail.com>
2026-08-08 13:18:19 +0500

    Class Created and Login Done

---
+++ Instagram_Follower_Bot/follower_bot.py
@@ -0,0 +1,42 @@
+SIMILAR_ACCOUNT="chefsteps"
+USERNAME="ahmadfkwj@gmail.com"
+PASSWORD="-b5KfZ5bGSMS-LI3"
+LOGIN_URL="https://app.100daysofpython.dev/services/share-a-naan/login"
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+## Keep the Browser Open:
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+
+class InstaFollower:
+    def __init__(self):
+        ## Set up the Chrome Driver:
+        self.driver = webdriver.Chrome(options=chrome_options)
+        self.driver.get(LOGIN_URL)
+        self.driver.fullscreen_window()
+
+    def login(self):
+        input_username = self.driver.find_element(By.ID , value="username")
+        input_password = self.driver.find_element(By.ID, value="password")
+        login_button = self.driver.find_element(By.CLASS_NAME , value="naan-btn-primary")
+
+        input_username.send_keys(USERNAME)
+        input_password.send_keys(PASSWORD)
+        login_button.click()
+
+        ## Save Info Pop up Dismiss
+        popup = self.driver.find_element(By.CLASS_NAME , value="naan-popup-dismiss")
+        popup.click()
+        popup.click()
+
+    def find_followers(self):
+        pass
+
+
+    def follow(self):
+        pass
+
---
+++ Instagram_Follower_Bot/main.py
@@ -0,0 +1,13 @@
+from follower_bot import InstaFollower
+
+## Opening the Website and Setting Up the Driver:
+bot = InstaFollower()
+
+
+bot.login()
+
+
+
+
+
+
