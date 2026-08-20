16405a357fd01814898539d414508837008ac224
AFK-08 <ahmadfkwj@gmail.com>
2026-08-20 11:56:43 +0500

    Added Some Kwargs

--- Jinja/server.py
+++ Jinja/server.py
@@ -1,13 +1,17 @@
 ## Dynamic Html pages
-
-
 from flask import Flask, render_template
+import random,time
+from datetime import datetime
+
 
 app = Flask(__name__)
 
 @app.route("/")
+
 def home():
-    return render_template("index.html")
+    current_year = datetime.now().year
+    random_number = random.randint(1,10)
+    return render_template("index.html",num=random_number,year=current_year)
 
 if __name__ == "__main__":
     app.run(debug=True)
