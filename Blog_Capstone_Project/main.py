fdbd2f482db49eb28245570de757ff781d8a5c81
AFK-08 <ahmadfkwj@gmail.com>
2026-08-21 12:07:14 +0500

    Build different urls for diff posts

--- Blog_Capstone_Project/main.py
+++ Blog_Capstone_Project/main.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    all_posts = response.json()
+    return render_template("index.html",posts=all_posts)
+
+@app.route("/post/<int:id>")
+def get_post(id):
+    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    all_posts = response.json()
+    return render_template("post.html", posts=all_posts, id=id)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
