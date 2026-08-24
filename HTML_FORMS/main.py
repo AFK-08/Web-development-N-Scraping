a56d5720e139f46bb8a698274f8a64b4dae62c24
AFK-08 <ahmadfkwj@gmail.com>
2026-08-24 21:26:20 +0500

    Got hold of the data which flask server recieves

--- HTML_FORMS/main.py
+++ HTML_FORMS/main.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
+@app.route("/login", methods=["POST"])
+def recieve_data():
+    username_data = request.form.get("username")
+    password_data = request.form.get("password")
+    return render_template("login.html",username=username_data,password=password_data)
 
 
 
