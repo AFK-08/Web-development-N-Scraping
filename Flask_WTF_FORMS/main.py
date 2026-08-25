4efca77c299fc43984a8fc0b7df2a2cba84f87f1
AFK-08 <ahmadfkwj@gmail.com>
2026-08-25 17:06:03 +0500

    Added the Validations

--- Flask_WTF_FORMS/main.py
+++ Flask_WTF_FORMS/main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import DataRequired,Email,Length
 
 class Login_Form(FlaskForm):
-        email = StringField(label='Email')
-        password = PasswordField(label='Password')
+        email = StringField(label='Email',validators=[DataRequired(),Email(message="Enter valid Email")])
+
+        password = PasswordField(label='Password',validators=[DataRequired(),Length(min=8,message="Enter minimum 8 characters")])
+        
         submit = SubmitField(label="LOGIN")
 
 app = Flask(__name__)
@@ -14,9 +17,12 @@ app.secret_key = 'mysecretkey'
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+
+
+@app.route("/login",methods=["GET","POST"]) 
 def login():
     form = Login_Form()
+    form.validate_on_submit()
     return  render_template("login.html", form=form)
 
 if __name__ == '__main__':
