4a52abec118d2360021a567e889bda22bb5ef03e
AFK-08 <ahmadfkwj@gmail.com>
2026-08-25 22:37:41 +0500

    Created a table with the fields

--- SQLITE_DB/main.py
+++ SQLITE_DB/main.py
@@ -1,3 +1,7 @@
 import sqlite3
 
 database = sqlite3.connect("./SQLITE_DB/books_collection.db")
+
+cursor = database.cursor()
+
+cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
