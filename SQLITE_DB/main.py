064d5089ec3a691e6c33d830675678a553a638e9
AFK-08 <ahmadfkwj@gmail.com>
2026-08-25 22:49:02 +0500

    Added a record into a Database

--- SQLITE_DB/main.py
+++ SQLITE_DB/main.py
@@ -4,4 +4,7 @@ database = sqlite3.connect("./SQLITE_DB/books_collection.db")
 
 cursor = database.cursor()
 
-cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+
+cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+database.commit()
