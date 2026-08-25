a6e4b478d6a2fdd83da0656b3ed4cf173e2663dd
AFK-08 <ahmadfkwj@gmail.com>
2026-08-25 16:20:56 +0500

    Some code improvements done

--- Flask_WTF_FORMS/main.py
+++ Flask_WTF_FORMS/main.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField,PasswordField,SubmitField
 
 class Login_Form(FlaskForm):
-        email = StringField('Email')
-        password = StringField('Password')
-
+        email = StringField(label='Email')
+        password = PasswordField(label='Password')
+        submit = SubmitField(label="LOGIN")
 
 app = Flask(__name__)
 app.secret_key = 'mysecretkey'
@@ -18,7 +18,6 @@ def home():
 def login():
     form = Login_Form()
     return  render_template("login.html", form=form)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
