f62464a5dddc95437fe115ac77a5cffe1d7918f5
AFK-08 <ahmadfkwj@gmail.com>
2026-08-26 10:53:40 +0500

    Why SQLALChemy Created? Read

--- SQLALCHEMY/main.py
+++ SQLALCHEMY/main.py
@@ -1,1 +1,5 @@
-## STarting SQLALCHEMY Project
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+
